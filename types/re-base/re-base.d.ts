cd896c8ee607825bf6862a46c23caadabcb98fc9
Scott O'Malley <scottyomalley@gmail.com>
2017-05-12 00:24:17 +0100

    Added reference to firebase types

--- types/re-base/re-base.d.ts
+++ types/re-base/re-base.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/gaearon/react-side-effect
 // Definitions by: Remo H. Jansen <https://github.com/remojansen/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
+///<reference types="firebase" />
 interface FirebaseConfiguration {
     /**
      * Your firebase API key
@@ -29,90 +29,90 @@ interface FirebaseConfiguration {
      */
     messagingSenderId?: string
 }
-
 interface SyncStateOptions {
-    context: object // The context of your component
-    state: string // The state property you want to sync with Firebase
-    asArray?: boolean //  Returns the Firebase data at the specified endpoint as an Array instead of an Object - asString: boolean //  Sets state as empty string instead of empty Object or Array if there is no Firebase data
-    isNullable?: boolean //  Sets state as null instead of empty Object or Array if there is no Firebase data
-    keepKeys?: boolean //  will keep any firebase generated keys intact when manipulating data using the asArray option.
-    queries?: object //  Queries to be used with your read operations. See Query Options for more details.
-    then?: () => void //  The callback function that will be invoked when the initial listener is established with Firebase. Typically used (with syncState) to change this.state.loading to false.
-    onFailure?: () => void //  A callback function that will be invoked if the current user does not have read or write permissions at the location.
+    context: object
+    state: string
+    asArray?: boolean
+    isNullable?: boolean
+    keepKeys?: boolean
+    queries?: object
+    then?: () => void
+    onFailure?: () => void
 }
 interface BindToStateOptions {
-    context: object // The context of your component
-    state: string // The state property you want to sync with Firebase
-    asArray?: boolean //  Returns the Firebase data at the specified endpoint as an Array instead of an Object - asString: boolean //  Sets state as empty string instead of empty Object or Array if there is no Firebase data
-    queries?: object //  Queries to be used with your read operations. See Query Options for more details.
-    then?: () => void //  The callback function that will be invoked when the initial listener is established with Firebase. Typically used (with syncState) to change this.state.loading to false.
-    onFailure?: () => void //  A callback function that will be invoked if the current user does not have read or write permissions at the location.
+    context: object
+    state: string
+    asArray?: boolean
+    queries?: object
+    then?: () => void
+    onFailure?: () => void
 }
 
 interface ListenToOptions {
-    context: object // The context of your component
-    asArray?: boolean //  Returns the Firebase data at the specified endpoint as an Array instead of an Object - asString: boolean //  Sets state as empty string instead of empty Object or Array if there is no Firebase data
-    then: (result : any) => void //  The callback function that will be invoked when the initial listener is established with Firebase. Typically used (with syncState) to change this.state.loading to false.
-    onFailure?: (error : any) => void //  A callback function that will be invoked if the current user does not have read or write permissions at the location.
-    queries?: object //  Queries to be used with your read operations. See Query Options for more details.
+    context: object
+    asArray?: boolean
+    then: (result : any) => void
+    onFailure?: (error : any) => void
+    queries?: object
 }
 
 interface FetchOptions {
-    context: object // The context of your component
-    asArray?: boolean //  Returns the Firebase data at the specified endpoint as an Array instead of an Object - asString: boolean //  Sets state as empty string instead of empty Object or Array if there is no Firebase data
-    then?: (result : any) => void //  The callback function that will be invoked when the initial listener is established with Firebase. Typically used (with syncState) to change this.state.loading to false.
-    onFailure?: () => void //  A callback function that will be invoked if the current user does not have read or write permissions at the location.
-    queries?: object //  Queries to be used with your read operations. See Query Options for more details.
+    context: object
+    asArray?: boolean
+    then?: (result : any) => void
+    onFailure?: () => void
+    queries?: object
+}
+
+interface PostOptions {
+    data: any
+    then?: (result : any) => void
+}
+interface PushOptions {
+    data: any
+    then?: (result : any) => void
+}
+
+interface UpdateOptions {
+    data: any
+    then?: (result : any) => void
 }
 
 interface RebaseBinding {}
 
+interface RebaseCredentials{
+    email: string,
+    password: string
+}
+
 interface Rebase {
-    /**
-     * Deletes the instance of re-base returned from Rebase.createClass, removing all the listeners that were added by the instance, and the underlying firebase app that was created. Note: You cannot re-initialize an app of the same name after it has been deleted.
-     */
+
     delete(callback?: () => void): void
 
-    /**
-     * Allows you to set up two way data binding between your component's state and your Firebase. Whenever your Firebase changes, your component's state will change. Whenever your component's state changes, Firebase will change.
-     */
     syncState(endpoint: string, options : SyncStateOptions) : RebaseBinding
 
-    /**
-     * One way data binding from Firebase to your component's state. Allows you to bind a component's state property to a Firebase endpoint so whenever that    Firebase endpoint changes, your component's state will be updated with that change.
-     */
     bindToState(endpoint: string, options : BindToStateOptions) : RebaseBinding
 
-    /**
-     * Allows you to listen to Firebase endpoints without binding those changes to a state property. Instead, a callback will be invoked with the newly updated data.
-     */
     listenTo(endpoint: string, options : ListenToOptions) : RebaseBinding
 
-    /**
-     * Allows you to retrieve the data from a Firebase endpoint just once without subscribing or listening for data changes.
-    */
-    fetch(endpoint: string, options : FetchOptions) : Promise<void>
+    fetch(endpoint: string, options : FetchOptions) : firebase.Promise<any>
 
-    //TODO: Method implementation
-    post()
-    //TODO: Method implementation
-    push()
-    //TODO: Method implementation
-    update()
-    //TODO: Method implementation
-    remove()
+    post(endpoint: string, options: PostOptions) : firebase.Promise<any>
 
-    /**
-     * Remove the listeners to Firebase when your component unmounts.
-     */
+    push(endpoint: string, options: PushOptions) : firebase.database.ThenableReference
+
+    update(endpoint: string, options: UpdateOptions) : firebase.Promise<any>
+
+    remove(endpoint: string, callback?: (result) => void) : firebase.Promise<any>
+
     removeBinding(ref : RebaseBinding): void
 
-    //TODO: Method implementation
-    reset()
+    reset(): void
+
+    authWithPassword(auth : RebaseCredentials, authHandler: (error : object | null, user: object) => void): void
 
 
 }
-
 declare module 're-base' {
     /**
      * Accepts a firebase configuration object as the first argument and an optional 'name' for the app as the second
--- types/re-base/re-base.tests.ts
+++ types/re-base/re-base.tests.ts
@@ -43,25 +43,101 @@ ref = base.bindToState('tasks', {
 base.removeBinding(ref)
 
 ref = base.listenTo('votes', {
-    context: this,
-    asArray: true,
-    then: (votesData) => {
-      var total = 0;
-      votesData.forEach((vote, index) => {
-        total += vote
-      });},
-    onFailure: () => {}
-  })
+      context: this,
+      asArray: true,
+      then: (votesData) => {
+            var total = 0;
+            votesData.forEach((vote, index) => {
+                  total += vote
+            });
+      },
+      onFailure: () => { }
+})
 base.removeBinding(ref)
 
 base.fetch('sales', {
-    context: this,
-    asArray: true,
-    then: (data) => {
-      console.log(data);
-    }
+      context: this,
+      asArray: true,
+      then: (data) => {
+            console.log(data);
+      }
 })
 
 base.fetch('sales', { context: this, asArray: true })
       .then(data => { console.log(data) })
       .catch(error => { /** handle error*/ })
+
+base.post(`users/123`, { data: { name: 'Tyler McGinnis', age: 25 } })
+      .then(() => { })
+      .catch(err => { });
+
+
+var immediatelyAvailableReference = base.push('bears', {
+      data: { name: 'George', type: 'Grizzly' },
+      then(err) {
+            console.log(err);
+      }
+});
+//available immediately, you don't have to wait for the callback to be called
+var generatedKey = immediatelyAvailableReference.key;
+
+immediatelyAvailableReference = base.push('bears', {
+      data: { name: 'George', type: 'Grizzly' }
+}).then(newLocation => {
+      var generatedKey = newLocation.key;
+}).catch(err => {
+      //handle error
+});
+//available immediately, you don't have to wait for the Promise to resolve
+var generatedKey = immediatelyAvailableReference.key;
+
+
+base.update('bears', {
+      data: { name: 'George' },
+      then(err) {
+            if (!err) {
+                  //bears endpint is now {name: 'George', type: 'Grizzly'}
+            }
+      }
+});
+
+// bears endpoint currently holds the object { name: 'Bill', type: 'Grizzly' }
+base.update('bears', {
+      data: { name: 'George' }
+}).then(() => {
+
+}).catch(err => {
+      //handle error
+});
+
+
+base.remove('bears', function (err) {
+      if (!err) {}
+});
+
+base.remove('bears')
+.then(() => {})
+.catch(error => {});
+
+base.syncState('users', {
+  context: this,
+  state: 'users',
+  asArray: true,
+  queries: {
+    orderByChild: 'iq',
+    limitToLast: 3,
+    endAt: ''
+  }
+})
+
+var authHandler = function(error, user) {
+  if(user){
+      console.log(user);
+  }
+}
+
+// Simple email/password authentication
+base.authWithPassword({
+  email    : 'bobtony@firebase.com',
+  password : 'correcthorsebatterystaple'
+}, authHandler);
